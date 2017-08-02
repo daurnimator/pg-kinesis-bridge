745759c75da397e3a46f89c3681f1dbbc45e8791
daurnimator <quae@daurnimator.com>
2017-08-02 16:10:26 +1000

    pg-kinesis-bridge.js: escape listen channel

--- pg-kinesis-bridge.js
+++ pg-kinesis-bridge.js
@@ -1,6 +1,14 @@
 const pg = require("pg");
 const AWS = require("aws-sdk");
 
+const escape_sql_identifier = function(s) {
+	/* Produce a "quoted identifier" https://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS */
+	/* Quoted identifiers can contain any character, except the character with code zero. (To include a double quote, write two double quotes.) */
+	if (s.indexOf("\0") != -1) throw Error("invalid identifier");
+	s = s.replace('"', '""');
+	return '"' + s + '"'
+};
+
 exports.run = function(pg_config, kinesis_config, channel, streamName, SequenceNumber) {
 	const client = new pg.Client(pg_config);
 	const kinesis = new AWS.Kinesis(kinesis_config);
@@ -28,6 +36,6 @@ exports.run = function(pg_config, kinesis_config, channel, streamName, SequenceN
 			client.on("notification", onnotify);
 
 			/* Subscribe to channel(s) */
-			return client.query("LISTEN " + channel); /* TODO: escaping */
+			return client.query('LISTEN ' + escape_sql_identifier(channel));
 		});
 };
